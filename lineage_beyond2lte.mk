ccb98ce3c914c8033b96728ca5f86deeb300cf66
Nathan Moore <124376269+Mgm2023@users.noreply.github.com>
2023-06-04 22:24:04 +0100

    beyond2lte: lineage_beyond2lte: add support for viper fx

--- lineage_beyond2lte.mk
+++ lineage_beyond2lte.mk
@@ -28,6 +28,8 @@ TARGET_HAVE_FOD := true
 ## Inherit from beyond2lte device
 $(call inherit-product, device/samsung/beyond2lte/device.mk)
 
+$(call inherit-product, device/samsung/exynos9820-common/v4afx/config.mk)
+
 WITH_GAPPS := 0
 # Valid for core/full GAPPS (default=true)
 TARGET_USE_GOOGLE_TELEPHONY := false
